677011645250404363827ae65ad8b53150c3f950
isaac <isaac@PYRITE>
2024-05-27 13:14:46 -0500

    Add project files.

---
+++ Pandacap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pandacap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
