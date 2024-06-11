1fcf815006837724ec9a0fbf62ded94a5f5ee22b
Isaac Schemm <isaacschemm@gmail.com>
2024-06-10 21:21:13 -0500

    Implement Atom/RSS feed fetch function

--- Pandacap.Functions/FeedHourly.cs
+++ Pandacap.Functions/FeedHourly.cs
@@ -1,14 +1,18 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Pandacap.Data;
 using Pandacap.HighLevel;
 
 namespace Pandacap.Functions
 {
-    public class FeedHourly()
+    public class FeedHourly(AtomRssFeedReader atomRssFeedReader, PandacapDbContext context)
     {
         [Function("FeedHourly")]
         public async Task Run([TimerTrigger("0 50 * * * *")] TimerInfo myTimer)
         {
-            
+            var feeds = await context.Feeds.Select(f => new { f.Id }).ToListAsync();
+            foreach (var feed in feeds)
+                await atomRssFeedReader.ReadFeedAsync(feed.Id);
         }
     }
 }
