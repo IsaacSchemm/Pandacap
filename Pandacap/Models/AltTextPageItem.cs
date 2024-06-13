6aee9aafc32ff28a05a5432e39e5befb7988222f
Isaac Schemm <isaacschemm@gmail.com>
2024-06-13 11:10:22 -0500

    Start building alt text UI

---
+++ Pandacap/Models/AltTextPageItem.cs
@@ -0,0 +1,21 @@
+﻿using DeviantArtFs.Extensions;
+using DeviantArtFs.ResponseTypes;
+using Pandacap.Data;
+
+namespace Pandacap.Models
+{
+    public record AltTextPageItem(
+        Deviation Deviation,
+        string? AltText) : IThumbnail
+    {
+        private record Thumb(Preview Preview) : IThumbnailRendition
+        {
+            string? IThumbnailRendition.Url => Preview.src;
+            int IThumbnailRendition.Width => Preview.width;
+            int IThumbnailRendition.Height => Preview.height;
+        }
+
+        IEnumerable<IThumbnailRendition> IThumbnail.Renditions =>
+            Deviation.thumbs.OrEmpty().Select(t => new Thumb(t));
+    };
+}
---
+++ Pandacap/Models/AltTextPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Pandacap.Models
+{
+    public class AltTextPageViewModel
+    {
+        public int? PrevOffset { get; set; }
+
+        public int? NextOffset { get; set; }
+
+        public IReadOnlyList<AltTextPageItem> Items { get; set; } = [];
+    }
+}
