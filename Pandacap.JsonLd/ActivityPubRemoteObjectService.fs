ac3f8d6c1bc48ac8b1fea1a049e55f69f0919074
Isaac Schemm <isaacschemm@gmail.com>
2024-09-13 13:28:19 -0500

    Make JSON-LD code more readable by splitting out smaller functions

--- Pandacap.JsonLd/ActivityPubRemoteObjectService.fs
+++ Pandacap.JsonLd/ActivityPubRemoteObjectService.fs
@@ -7,16 +7,28 @@ open JsonLD.Core
 open Pandacap.HighLevel
 
 type ActivityPubRemoteObjectService(requestHandler: ActivityPubRequestHandler) =
-    let arr (name: string) (obj: JToken) = obj[name]
+    /// Given an expanded object and a field name, extracts the JSON array value.
+    let list (name: string) (obj: JToken) = obj[name]
 
-    let single name obj = Seq.exactlyOne (arr name obj)
-    let str name obj = (arr name obj).Value<string>()
+    /// Given a sequence of objects and a field name, extracts the JSON array values for each object and combines them into a single sequence.
+    let combine name items = Seq.collect (list name) items
 
-    let id obj = obj |> str "@id"
-    let value obj = obj |> str "@value"
+    /// Given an expanded object and a field name, extracts the first or only value in the array.
+    let single name obj = Seq.head (list name obj)
 
-    let firstOrNull seq = seq |> Seq.tryHead |> Option.toObj
+    /// Given an expanded object and a field name, extracts the string value.
+    let str name obj = (list name obj).Value<string>()
 
+    /// Given an expanded object, extracts the node identifier as a string.
+    let node_id obj = obj |> str "@id"
+
+    /// Given an expanded object, extracts the value as a string.
+    let node_value obj = obj |> str "@value"
+
+    /// Given a sequence of objects, returns the result of running the function on the first object, or returns null if the sequence is empty.
+    let first func arr = arr |> Seq.map func |> Seq.tryHead |> Option.toObj
+
+    /// A cache of already-retrieved actors within the lifetime of this service object.
     let actorCache = Map.empty<string, RemoteActor>
 
     member _.FetchActorAsync(url: string, cancellationToken: CancellationToken) = task {
@@ -26,35 +38,35 @@ type ActivityPubRemoteObjectService(requestHandler: ActivityPubRequestHandler) =
             let uri = new Uri(url)
             let! json = requestHandler.GetJsonAsync(uri, cancellationToken)
 
-            let object = json |> JObject.Parse |> JsonLdProcessor.Expand |> Seq.exactlyOne
-
-            let object_id = object |> id
-            let inbox = object |> single "http://www.w3.org/ns/ldp#inbox" |> id
-
-            let sharedInbox = firstOrNull [
-                for endpoint in object |> arr "https://www.w3.org/ns/activitystreams#endpoints" do
-                    for sharedInbox in endpoint |> arr "https://www.w3.org/ns/activitystreams#sharedInbox" do
-                        sharedInbox |> id
-            ]
-
-            let preferredUsername = object |> single "https://www.w3.org/ns/activitystreams#preferredUsername" |> value
-            let iconUrl = firstOrNull [
-                for icon in object |> arr "https://www.w3.org/ns/activitystreams#icon" do
-                    for url in icon |> arr "https://www.w3.org/ns/activitystreams#url" do
-                        url |> id
-            ]
-
-            let keyId = object |> single "https://w3id.org/security#publicKey" |> id
-            let keyPem = object |> single "https://w3id.org/security#publicKey" |> single "https://w3id.org/security#publicKeyPem" |> value
+            let object =
+                json
+                |> JObject.Parse
+                |> JsonLdProcessor.Expand
+                |> Seq.exactlyOne
 
             return new RemoteActor(
-                object_id,
-                inbox,
-                sharedInbox,
-                preferredUsername,
-                iconUrl,
-                keyId,
-                keyPem)
+                node_id object,
+                object
+                    |> list "http://www.w3.org/ns/ldp#inbox"
+                    |> first node_id,
+                object
+                    |> list "https://www.w3.org/ns/activitystreams#endpoints"
+                    |> combine "https://www.w3.org/ns/activitystreams#sharedInbox"
+                    |> first node_id,
+                object
+                    |> list "https://www.w3.org/ns/activitystreams#preferredUsername"
+                    |> first node_value,
+                object
+                    |> list "https://www.w3.org/ns/activitystreams#icon"
+                    |> combine "https://www.w3.org/ns/activitystreams#url"
+                    |> first node_id,
+                object
+                    |> list "https://w3id.org/security#publicKey"
+                    |> first node_id,
+                object
+                    |> list "https://w3id.org/security#publicKey"
+                    |> combine "https://w3id.org/security#publicKeyPem"
+                    |> first node_value)
     }
 
     member this.FetchActorAsync(url) =
