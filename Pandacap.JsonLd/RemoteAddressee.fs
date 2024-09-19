d2f083b427d1280bf59ea64526351d5b587bb7b1
Isaac Schemm <isaacschemm@gmail.com>
2024-09-19 13:54:04 -0500

    Show type of objects in To and CC

--- Pandacap.JsonLd/RemoteAddressee.fs
+++ Pandacap.JsonLd/RemoteAddressee.fs
@@ -6,6 +6,12 @@ type RemoteAddressee =
 | Actor of RemoteActor
 | InaccessibleObject of id: string
 with
+    member this.Type =
+        match this with
+        | PublicCollection -> "Public"
+        | Actor a -> a.Type.Replace("https://www.w3.org/ns/activitystreams#", "")
+        | Collection _ -> "Collection"
+        | InaccessibleObject _ -> "Inaccessible or malformed actor object"
     member this.Id =
         match this with
         | PublicCollection -> "https://www.w3.org/ns/activitystreams#Public"
