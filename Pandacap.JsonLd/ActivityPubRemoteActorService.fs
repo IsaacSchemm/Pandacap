70adcb00e9dc9359b844b44599451f1f5a0d9eab
Isaac Schemm <isaacschemm@gmail.com>
2024-09-19 11:27:23 -0500

    Add error handling for invalid JSON-LD (e.g. Akkoma)

--- Pandacap.JsonLd/ActivityPubRemoteActorService.fs
+++ Pandacap.JsonLd/ActivityPubRemoteActorService.fs
@@ -7,56 +7,62 @@ open JsonLD.Core
 open Pandacap.HighLevel
 
 type ActivityPubRemoteActorService(requestHandler: ActivityPubRequestHandler) =
-    let actorCache = Map.empty<string, RemoteActor>
+    let mutable actorCache = Map.empty<string, RemoteActor>
+
+    let fetchAsync (url: string) (cancellationToken: CancellationToken) = task {
+        let uri = new Uri(url)
+        let! json = requestHandler.GetJsonAsync(uri, cancellationToken)
+
+        let object =
+            json
+            |> JObject.Parse
+            |> JsonLdProcessor.Expand
+            |> Seq.exactlyOne
+
+        return {
+            Type = object
+                |> node_type
+                |> Seq.head
+            Id =
+                object
+                |> node_id
+            Inbox =
+                object
+                |> list "http://www.w3.org/ns/ldp#inbox"
+                |> first node_id
+            SharedInbox =
+                object
+                |> list "https://www.w3.org/ns/activitystreams#endpoints"
+                |> combine "https://www.w3.org/ns/activitystreams#sharedInbox"
+                |> first node_id
+            PreferredUsername =
+                object
+                |> list "https://www.w3.org/ns/activitystreams#preferredUsername"
+                |> first node_value
+            IconUrl =
+                object
+                |> list "https://www.w3.org/ns/activitystreams#icon"
+                |> combine "https://www.w3.org/ns/activitystreams#url"
+                |> first node_id
+            KeyId =
+                object
+                |> list "https://w3id.org/security#publicKey"
+                |> first node_id
+            KeyPem =
+                object
+                |> list "https://w3id.org/security#publicKey"
+                |> combine "https://w3id.org/security#publicKeyPem"
+                |> first node_value
+        }
+    }
 
     member _.FetchActorAsync(url: string, cancellationToken: CancellationToken) = task {
         match actorCache.TryFind(url) with
         | Some result -> return result
         | None ->
-            let uri = new Uri(url)
-            let! json = requestHandler.GetJsonAsync(uri, cancellationToken)
-
-            let object =
-                json
-                |> JObject.Parse
-                |> JsonLdProcessor.Expand
-                |> Seq.exactlyOne
-
-            return {
-                Type = object
-                    |> node_type
-                    |> Seq.head
-                Id =
-                    object
-                    |> node_id
-                Inbox =
-                    object
-                    |> list "http://www.w3.org/ns/ldp#inbox"
-                    |> first node_id
-                SharedInbox =
-                    object
-                    |> list "https://www.w3.org/ns/activitystreams#endpoints"
-                    |> combine "https://www.w3.org/ns/activitystreams#sharedInbox"
-                    |> first node_id
-                PreferredUsername =
-                    object
-                    |> list "https://www.w3.org/ns/activitystreams#preferredUsername"
-                    |> first node_value
-                IconUrl =
-                    object
-                    |> list "https://www.w3.org/ns/activitystreams#icon"
-                    |> combine "https://www.w3.org/ns/activitystreams#url"
-                    |> first node_id
-                KeyId =
-                    object
-                    |> list "https://w3id.org/security#publicKey"
-                    |> first node_id
-                KeyPem =
-                    object
-                    |> list "https://w3id.org/security#publicKey"
-                    |> combine "https://w3id.org/security#publicKeyPem"
-                    |> first node_value
-            }
+            let! actor = fetchAsync url cancellationToken
+            actorCache <- actorCache |> Map.add url actor
+            return actor
     }
 
     member this.FetchActorAsync(url) =
@@ -66,11 +72,15 @@ type ActivityPubRemoteActorService(requestHandler: ActivityPubRequestHandler) =
         if url = PublicCollection.Id then
             return PublicCollection
         else
-            let! actor = this.FetchActorAsync(url, cancellationToken)
-            match actor.Type with
-            | "https://www.w3.org/ns/activitystreams#Collection"
-            | "https://www.w3.org/ns/activitystreams#OrderedCollection" ->
-                return Collection url
+            try
+                let! actor = this.FetchActorAsync(url, cancellationToken)
+                match actor.Type with
+                | "https://www.w3.org/ns/activitystreams#Collection"
+                | "https://www.w3.org/ns/activitystreams#OrderedCollection" ->
+                    return Collection url
+                | _ ->
+                    return Actor actor
+            with
             | _ ->
-                return Actor actor
+                return InaccessibleObject url
     }
--- Pandacap.JsonLd/RemoteAddressee.fs
+++ Pandacap.JsonLd/RemoteAddressee.fs
@@ -4,9 +4,11 @@ type RemoteAddressee =
 | PublicCollection
 | Collection of id: string
 | Actor of RemoteActor
+| InaccessibleObject of id: string
 with
     member this.Id =
         match this with
         | PublicCollection -> "https://www.w3.org/ns/activitystreams#Public"
         | Actor a -> a.Id
-        | Collection id -> id
+        | Collection id
+        | InaccessibleObject id -> id
