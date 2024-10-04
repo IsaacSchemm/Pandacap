5009f80a28bfc21e8795ece88027931e7ae0f58c
Isaac Schemm <isaacschemm@gmail.com>
2024-10-04 12:53:40 -0500

    (#26) Fix Bluesky profile link for shares

--- Pandacap.LowLevel/ApplicationInformation.fs
+++ Pandacap.LowLevel/ApplicationInformation.fs
@@ -25,7 +25,7 @@ type ApplicationInformation = {
     member _.ApplicationName = "Pandacap"
 
     /// The Pandacap version number.
-    member _.VersionNumber = "3.4.5"
+    member _.VersionNumber = "3.4.6"
 
     /// A URL to a website with more information about the application.
     member _.WebsiteUrl = "https://github.com/IsaacSchemm/Pandacap"
