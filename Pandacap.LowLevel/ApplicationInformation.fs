693b1302e28afb183d3c1273edabfd37dc676353
Isaac Schemm <isaacschemm@gmail.com>
2024-10-14 15:34:51 -0500

    Use big images in Discord embed

--- Pandacap.LowLevel/ApplicationInformation.fs
+++ Pandacap.LowLevel/ApplicationInformation.fs
@@ -21,7 +21,7 @@ type ApplicationInformation = {
     member _.ApplicationName = "Pandacap"
 
     /// The Pandacap version number.
-    member _.VersionNumber = "3.7.2"
+    member _.VersionNumber = "3.7.3"
 
     /// A URL to a website with more information about the application.
     member _.WebsiteUrl = "https://github.com/IsaacSchemm/Pandacap"
