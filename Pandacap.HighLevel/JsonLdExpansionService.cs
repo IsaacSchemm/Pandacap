94e5f0d3f31bdbb7336bbb26b5c066c06650f9fa
Isaac Schemm <isaacschemm@gmail.com>
2024-10-16 20:16:15 -0500

    Combine error handlers

--- Pandacap.HighLevel/JsonLdExpansionService.cs
+++ Pandacap.HighLevel/JsonLdExpansionService.cs
@@ -12,29 +12,10 @@ namespace Pandacap.HighLevel
             {
                 return JsonLdProcessor.Expand(jObject).Single();
             }
-            catch (JsonLdError ex) when (ex.GetType() == JsonLdError.Error.RecursiveContextInclusion)
-            {
-                // Remove ActivityStreams from the context
-                // Recursive context errors might be caused by another context including the ActivityStreams context
-                foreach (var token in jObject["@context"].ToList())
-                {
-                    if (token is JValue v)
-                        if (v.Value as string == "https://www.w3.org/ns/activitystreams")
-                            v.Remove();
-                }
-
-                // Retry
-                return JsonLdProcessor.Expand(jObject).Single();
-            }
-            catch (JsonLdError ex) when (ex.GetType() == JsonLdError.Error.LoadingRemoteContextFailed)
+            catch (JsonLdError ex) when (ex.GetType() == JsonLdError.Error.RecursiveContextInclusion || ex.GetType() == JsonLdError.Error.LoadingRemoteContextFailed)
             {
                 // Remove everything except ActivityStreams from the context
-                foreach (var token in jObject["@context"].ToList())
-                {
-                    if (token is JValue v)
-                        if (v.Value as string != "https://www.w3.org/ns/activitystreams")
-                            v.Remove();
-                }
+                jObject["@context"] = new JArray(new JValue("https://www.w3.org/ns/activitystreams"));
 
                 // Retry
                 return JsonLdProcessor.Expand(jObject).Single();
