8bd79a6eaadf774cbfa65b65f7ff4a504b0aa66e
Isaac Schemm <isaacschemm@gmail.com>
2025-05-18 22:01:34 -0500

    Simplify job

--- Pandacap.Functions/StarpassSync.cs
+++ Pandacap.Functions/StarpassSync.cs
@@ -9,25 +9,7 @@ namespace Pandacap.Functions
         [Function("StarpassSync")]
         public async Task Run([TimerTrigger("0 */30 * * * *")] TimerInfo myTimer)
         {
-            List<Exception> exceptions = [];
-
-            async Task c(Task t)
-            {
-                try
-                {
-                    await t;
-                }
-                catch (Exception e)
-                {
-                    exceptions.Add(e);
-                }
-            }
-
-            await c(starpassAgent.RefreshAllAsync(
-                newPostLimit: 1));
-
-            if (exceptions.Count > 0)
-                throw new AggregateException(exceptions);
+            await starpassAgent.RefreshAllAsync(newPostLimit: 1);
         }
     }
 }
