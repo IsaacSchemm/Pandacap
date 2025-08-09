274d3047ed011d624f5e7dc382e2d838ad488b50
Isaac Schemm <isaacschemm@gmail.com>
2025-08-09 17:12:45 -0500

    Move ComputerVisionProvider to C#

---
+++ Pandacap/ComputerVision.cs
@@ -0,0 +1,35 @@
+﻿using Azure.AI.Vision.ImageAnalysis;
+using Azure.Identity;
+
+namespace Pandacap
+{
+    public record ComputerVisionConfiguration(
+        string Endpoint,
+        string TenantId);
+
+    public class ComputerVisionProvider(ComputerVisionConfiguration config)
+    {
+        public async Task<string> AnalyzeImageAsync(byte[] data, CancellationToken cancellationToken)
+        {
+            var client = new ImageAnalysisClient(
+                new Uri(config.Endpoint),
+                new DefaultAzureCredential(
+                    new DefaultAzureCredentialOptions
+                    {
+                        TenantId = config.TenantId
+                    }));
+
+            var analysis = await client.AnalyzeAsync(
+                BinaryData.FromBytes(data),
+                VisualFeatures.Caption | VisualFeatures.Read,
+                cancellationToken: cancellationToken);
+
+            return string.Join("\n", [
+                analysis.Value.Caption.Text,
+                .. analysis.Value.Read.Blocks
+                    .SelectMany(block => block.Lines)
+                    .Select(block => block.Text)
+            ]);
+        }
+    }
+}
