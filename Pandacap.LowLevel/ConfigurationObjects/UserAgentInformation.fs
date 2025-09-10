a1ca3b48a79d250f9f7cccd58e01960ad9fce819
Isaac Schemm <isaacschemm@gmail.com>
2025-09-10 10:20:17 -0500

    Update version number

--- Pandacap.LowLevel/ConfigurationObjects/UserAgentInformation.fs
+++ Pandacap.LowLevel/ConfigurationObjects/UserAgentInformation.fs
@@ -5,7 +5,7 @@ module UserAgentInformation =
     let ApplicationName = "Pandacap"
 
     /// The Pandacap version number.
-    let VersionNumber = "13.1.0"
+    let VersionNumber = "13.2.0"
 
     /// A URL to a website with more information about the application.
     let WebsiteUrl = "https://github.com/IsaacSchemm/Pandacap"
