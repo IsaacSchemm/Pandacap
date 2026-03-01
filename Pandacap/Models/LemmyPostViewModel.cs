743074a5dbd05f0876da6835aa3af90940794ad6
Isaac Schemm <isaacschemm@gmail.com>
2026-02-28 21:17:37 -0600

    Rename model

--- Pandacap/Models/LemmyPostViewModel.cs
+++ Pandacap/Models/LemmyPostViewModel.cs
@@ -3,7 +3,7 @@ using Pandacap.Clients;
 
 namespace Pandacap.Models
 {
-    public record PostViewModel(
+    public record LemmyPostViewModel(
         Lemmy.Community Community,
         Lemmy.PostView PostView,
         FSharpList<Lemmy.CommentBranch> Comments);
