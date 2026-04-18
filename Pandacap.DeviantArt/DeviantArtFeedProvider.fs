50c69adcc190bbb458b32aa1a33e9f7a65edce03
Isaac Schemm <isaacschemm@gmail.com>
2026-04-18 17:13:36 -0500

    Fix for non-deterministic DeviantArt home feed

--- Pandacap.DeviantArt/DeviantArtFeedProvider.fs
+++ Pandacap.DeviantArt/DeviantArtFeedProvider.fs
@@ -1,35 +1,39 @@
 ﻿namespace Pandacap.DeviantArt
 
+open System
 open FSharp.Control
-open Pandacap.DeviantArt.Interfaces
 open DeviantArtFs.ParameterTypes
-open Pandacap.UI.Elements
+open Pandacap.DeviantArt.Interfaces
 open Pandacap.UI.Badges
+open Pandacap.UI.Elements
 
 type internal DeviantArtFeedProvider() =
+    let orElse defaultValue initial =
+        initial |> Option.defaultValue defaultValue
+
     interface IDeviantArtFeedProvider with
         member _.GetHomeFeedAsync(token, offset) = asyncSeq {
-            for item in DeviantArtFs.Api.Browse.GetHomeAsync token MaximumPagingLimit (PagingOffset offset) do
-                match item.title, item.published_time, item.url, item.author with
-                | Some title, Some time, Some url, Some author ->
-                    yield {
-                        new IPost with
-                            member _.Badge = Badges.DeviantArt
-                            member _.DisplayTitle = title
-                            member _.Id = $"{item.deviationid}"
-                            member _.InternalUrl = url
-                            member _.ExternalUrl = url
-                            member _.PostedAt = time
-                            member _.ProfileUrl = null
-                            member _.Thumbnails = seq {
-                                for thumbnail in item.thumbs |> Option.defaultValue [] do {
-                                    new IPostThumbnail with
-                                        member _.Url = thumbnail.src
-                                        member _.AltText = ""
-                                }
-                            }
-                            member _.Username = author.username
-                            member _.Usericon = author.usericon
+            let! page =
+                DeviantArtFs.Api.Browse.PageHomeAsync token MaximumPagingLimit (PagingOffset offset)
+                |> Async.AwaitTask
+
+            for item in page.results |> Option.defaultValue [] do {
+                new IPost with
+                    member _.Badge = Badges.DeviantArt
+                    member _.DisplayTitle = item.title |> orElse $"{item.deviationid}"
+                    member _.Id = $"{item.deviationid}"
+                    member _.InternalUrl = item.url |> orElse null
+                    member _.ExternalUrl = item.url |> orElse null
+                    member _.PostedAt = item.published_time |> orElse DateTimeOffset.MinValue
+                    member _.ProfileUrl = null
+                    member _.Thumbnails = seq {
+                        for thumbnail in item.thumbs |> Option.defaultValue [] do {
+                            new IPostThumbnail with
+                                member _.Url = thumbnail.src
+                                member _.AltText = ""
+                        }
                     }
-                | _ -> ()
+                    member _.Username = null
+                    member _.Usericon = null
+            }
         }
