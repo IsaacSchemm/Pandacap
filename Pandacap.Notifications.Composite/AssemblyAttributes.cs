a27ad0cb6d426dbf1874185a8f7dd162fffaf111
Isaac Schemm <isaacschemm@gmail.com>
2026-04-28 14:37:16 -0500

    Port CompositeNotificationHandler to F#

--- Pandacap.Notifications.Composite/AssemblyAttributes.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Pandacap.Notifications.Composite.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ Pandacap.Notifications.Composite/AssemblyAttributes.fs
@@ -0,0 +1,7 @@
+namespace Pandacap.ActivityPub.Services
+
+open System.Runtime.CompilerServices
+
+[<assembly:InternalsVisibleTo("Pandacap.Notifications.Composite.Tests")>]
+[<assembly:InternalsVisibleTo("DynamicProxyGenAssembly2")>]
+()
