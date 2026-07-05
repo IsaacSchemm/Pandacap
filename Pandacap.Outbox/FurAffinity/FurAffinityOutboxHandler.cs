568021541c48f233eda78e98d6109c14dfa2496e
Isaac Schemm <isaacschemm@gmail.com>
2026-07-05 18:10:01 -0500

    Add FA uploading code

--- Pandacap.Outbox/FurAffinity/FurAffinityOutboxHandler.cs
+++ Pandacap.Outbox/FurAffinity/FurAffinityOutboxHandler.cs
@@ -1,4 +1,5 @@
-﻿using Pandacap.Database;
+﻿using Microsoft.EntityFrameworkCore;
+using Pandacap.Database;
 using Pandacap.FurAffinity.Interfaces;
 using Pandacap.Outbox.Interfaces;
 
@@ -7,11 +8,83 @@ namespace Pandacap.Outbox.FurAffinity
     internal class FurAffinityOutboxHandler(
         IFurAffinityClientFactory furAffinityClientFactory,
         IEnumerable<IFurAffinityCredentials> furAffinityCredentials,
+        IHttpClientFactory httpClientFactory,
         PandacapDbContext pandacapDbContext) : IOutboxDestination
     {
+        private async Task<byte[]?> TryGetImageDataAsync(Post post)
+        {
+            foreach (var image in post.Images)
+            {
+                if (post.GetImageUrl(image) is string url)
+                {
+                    using var client = httpClientFactory.CreateClient();
+                    using var resp = await client.GetAsync(url);
+                    return await resp.EnsureSuccessStatusCode().Content.ReadAsByteArrayAsync();
+                }
+            }
+
+            return null;
+        }
+
         public async Task<bool> PublishNextQueuedPostAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var credentials = furAffinityCredentials.FirstOrDefault();
+
+            if (credentials == null)
+                return false;
+
+            var post = await pandacapDbContext.Posts
+                .Where(x => x.QueuedFurAffinityPost != null)
+                .OrderByDescending(x => x.PublishedTime)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (post == null)
+                return false;
+
+            var queued = post.QueuedFurAffinityPost!;
+
+            var client = furAffinityClientFactory.CreateClient(credentials, Pandacap.FurAffinity.Models.Domain.WWW);
+
+            post.FurAffinityUsername = await client.WhoamiAsync(cancellationToken);
+            post.FurAffinitySubmissionId = null;
+            post.FurAffinityJournalId = null;
+
+            if (await TryGetImageDataAsync(post) is byte[] imageData)
+            {
+                Uri posted = await client.PostArtworkAsync(
+                    imageData,
+                    new Pandacap.FurAffinity.Models.ArtworkMetadata(
+                        post.Title,
+                        post.Body,
+                        [.. post.Tags],
+                        queued.Cat,
+                        queued.Scrap,
+                        queued.Atype,
+                        queued.Species,
+                        queued.Gender,
+                        queued.Rating,
+                        queued.LockComments,
+                        [.. queued.FolderIds]),
+                    cancellationToken);
+
+                post.FurAffinitySubmissionId = int.Parse(posted.Segments[2].TrimEnd('/'));
+            }
+            else
+            {
+                Uri posted = await client.PostJournalAsync(
+                    post.Title,
+                    post.Body,
+                    queued.Rating,
+                    cancellationToken);
+
+                post.FurAffinityJournalId = int.Parse(posted.Segments[2].TrimEnd('/'));
+            }
+
+            post.QueuedFurAffinityPost = null;
+
+            await pandacapDbContext.SaveChangesAsync(cancellationToken);
+
+            return true;
         }
 
         public async Task SynchronizeOfflinePlatformCacheAsync(CancellationToken cancellationToken)
@@ -21,7 +94,7 @@ namespace Pandacap.Outbox.FurAffinity
             if (credentials == null)
                 return;
 
-            var client = furAffinityClientFactory.CreateClient(credentials, Pandacap.FurAffinity.Models.Domain.SFW);
+            var client = furAffinityClientFactory.CreateClient(credentials, Pandacap.FurAffinity.Models.Domain.WWW);
 
             var folders = await client.ListGalleryFoldersAsync(cancellationToken);
 
